9e1e5f74347c73a6ecd806ba5bde22eade918ef9
weiwuprojects <chana9@uci.edu>
2018-12-23 22:58:44 -0800

    fix next.js config

--- next.config.js
+++ next.config.js
@@ -15,3 +15,37 @@ module.exports = withCSS({
       return config
     }
 })
+
+// const { PHASE_PRODUCTION_SERVER } =
+//   process.env.NODE_ENV === 'development'
+//     ? {}
+//     : !process.env.NOW_REGION
+//       ? require('next/constants')
+//       : require('next-server/constants');
+
+// module.exports = (phase, { defaultConfig }) => {
+//   if (phase === PHASE_PRODUCTION_SERVER) {
+    
+//     return {
+//       /* production only config */
+//     }
+//   }
+
+//   const webpack = require('webpack')
+//   const withCSS = require('@zeit/next-css')
+//   return withCSS({
+//     webpack: function (config) {
+//       config.module.rules.push({
+//         test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+//         use: {
+//           loader: 'url-loader',
+//           options: {
+//             limit: 100000,
+//             name: '[name].[ext]'
+//           }
+//         }
+//       })
+//       return config
+//     }
+// })
+// }
